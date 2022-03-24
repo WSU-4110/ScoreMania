0735460f1c56ee5e213a2bea8a83b578400c8aca
Omar Alsaed <omar.alsaed010@gmail.com>
2022-03-24 01:05:04 -0400

    Revert "Merge pull request #36 from WSU-4110/revert-34-omar-from-main"
    This reverts commit d9c73e791ef84b3db19d886b3058f1ad474f8375, reversing
    changes made to 655d9633f3c55cfc3e6994bad0579423fe3fa6c9.

---
+++ Assets/ScoreScript.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour
+{
+    private Text score;
+    private int HealthScore;
+    // Start is called before the first frame update
+    void Start()
+    {
+        HealthScore = 20;
+        score = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score.text = HealthScore.ToString();
+    }
+    public void AddScore(){
+        HealthScore += 1;
+    }
+    public void SubScore(){
+        HealthScore -= 1;
+    }
+    public void SubFive()
+    {
+        if(HealthScore != 0)
+        HealthScore -= 5;
+    }
+    public void Commander()
+    {
+        HealthScore = 40;
+    }
+    public void Reset()
+    {
+        HealthScore = 20;
+    }
+}
