418b807898ee40f7109a7bd5c4eaf640f907f161
Phillip Kaiser <ha4103@wayne.edu>
2022-04-19 22:37:16 -0400

    Added Return To menu Button in MTG

---
+++ Assets/Scripts/Magic Scripts/ReturnMenu.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LoadMainMenu()
+    {
+
+        Application.LoadLevel("MainMenu");
+
+    }
+}
