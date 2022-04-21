f537fdaacecdc92b83692437e52af58f5480790b
bhohentanner <98716088+bhohentanner@users.noreply.github.com>
2022-04-21 00:42:37 -0400

    new 4 player scene
    Added names and return button

---
+++ YuGiOh Scripts/Dce.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dce : MonoBehaviour
+{
+    Animator anim;
+
+    public Roller myRoller;
+    public bool One;
+    public void Roller()
+    {
+        if (One)
+            myRoller.RollDiceOneFuntion();
+
+    }
+}
---
+++ YuGiOh Scripts/PanelOpner.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelOpner : MonoBehaviour
+{
+    public GameObject Panel;
+
+    public void OpenPanel()
+    {
+
+        if (Panel != null)
+        {
+            Panel.SetActive(true);
+        }
+    }
+
+    public void ClosePanel()
+    {
+
+        if (Panel != null)
+        {
+            Panel.SetActive(false);
+        }
+    }
+}
