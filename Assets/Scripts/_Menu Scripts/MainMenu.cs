5a9ff5ca6399f23f006a9fdd4de08a2bc9e1d19e
MarufSourav <maruf.sourav99@gmail.com>
2022-04-21 02:10:51 -0400

    Main Menu YuGiOh Error Checking

--- Assets/Scripts/_Menu Scripts/MainMenu.cs
+++ Assets/Scripts/_Menu Scripts/MainMenu.cs
@@ -6,26 +6,25 @@ using UnityEngine.UI;
 public class MainMenu : MonoBehaviour
 {
     ToggleGroup toggleGroup;
-    public AmountOfPlayers aop;
+    public AmountOfPlayers aop;   
+    public GameObject YuGiOhSC;
+    public GameObject CB;
+    public GameObject SB;
     public int sceneIndex;
     void Start()
     {
         toggleGroup = GetComponent<ToggleGroup>();
         //calls to get currently selected values as the values selected on last run of the program persist since they're stored on a scriptable object
         GetGameName();
-        GetPlayerAmount();
+        GetPlayerAmount();        
     }
-
     public void GetGameName()
     {
         Toggle toggle = toggleGroup.ActiveToggles().FirstOrDefault();
         if (toggle.name == "MTG")
             aop.gameName = "MagicTheGathering";
         else if (toggle.name == "YGO")
-            aop.gameName = "YuGiOh";
-        
-        
-            
+            aop.gameName = "YuGiOh";            
     }
     public void GetPlayerAmount()
     {
@@ -36,11 +35,15 @@ public class MainMenu : MonoBehaviour
             aop.amountOfPlayers = 3;
         else if (toggle.name == "4Players")
             aop.amountOfPlayers = 4;
-    }
-   
-
+    }    
     public void GameStart()
     {
+        while (aop.gameName == "YuGiOh" && aop.amountOfPlayers == 3) 
+        {
+            YuGiOhSC.SetActive(true);
+            CB.SetActive(false);
+            SB.SetActive(false);            
+        }       
         if (aop.gameName == "MagicTheGathering" && aop.amountOfPlayers == 3)
             aop.gameName = "MTG3Player";
 
